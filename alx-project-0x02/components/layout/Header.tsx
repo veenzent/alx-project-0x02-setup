b39957421796e26b1e32d1f84fc4146cb7a9b193
veenzent <odumevincent19@gmail.com>
2025-11-19 13:41:58 +0100

    Basic Routing in Next.js task completed

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -4,20 +4,20 @@ export default function Header() {
   return (
     <header className="flex items-center justify-between bg-zinc-100 p-4">
       <h1 className="text-3xl text-blue font-bold">
-        <a href="/home" className="hover:text-zinc-900">
+        <Link href="/home" className="hover:text-zinc-900">
           Home
-        </a>
+        </Link>
       </h1>
       <nav className="flex items-center gap-4">
-        <a href="/about" className="hover:text-zinc-900">
+        <Link href="/about" className="hover:text-zinc-900">
           About
-        </a>
-        <a href="/posts" className="hover:text-zinc-900">
+        </Link>
+        <Link href="/posts" className="hover:text-zinc-900">
           Posts
-        </a>
-        <a href="/users" className="hover:text-zinc-900">
+        </Link>
+        <Link href="/users" className="hover:text-zinc-900">
           Users
-        </a>
+        </Link>
       </nav>
     </header>
   );
