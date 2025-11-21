ee2f9660adef994e64677c9b008a631ca7f22c51
veenzent <odumevincent19@gmail.com>
2025-11-21 13:03:15 +0100

    Implemented Navigation and Layout

---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,24 @@
+import Header from "@/components/layout/Header";
+import Button from "@/components/common/Button";
+
+export default function About() {
+    return (
+        <>
+            <Header />
+            <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+                <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+                    Posts
+                </h1>
+                <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+                    Hello, my name is Alex and I'm a software engineer from
+                    Ukraine. I'm passionate about building web applications with
+                    modern technologies like Next.js, TypeScript, and Tailwind
+                    CSS.
+                </p>
+                <Button size="small" shape="rounded-sm" />
+                <Button size="medium" shape="rounded-md" />
+                <Button size="large" shape="rounded-full" />
+            </main>
+        </>
+    );
+}
